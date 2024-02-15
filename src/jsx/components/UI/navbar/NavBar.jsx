ab8dc685e09431b01dcf5fd57fc3a1e86186dea8
VPresich <valya.alexandrova.presich@gmail.com>
2024-02-15 01:50:26 +0100

    add NavBar component

--- src/jsx/components/UI/navbar/NavBar.jsx
+++ src/jsx/components/UI/navbar/NavBar.jsx
@@ -1,18 +1,10 @@
-import { NavLink, Routes, Route } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { useState } from 'react';
-import { Lesson01 } from '../components/Lesson-01/Lesson-01';
-import { Lesson02 } from '../components/Lesson-02/Lesson-02';
-import { Lesson03 } from '../components/Lesson-03/Lesson-03';
-import { Lesson04 } from '../components/Lesson-04/Lesson-04';
-import { Lesson05 } from '../components/Lesson-05/Lesson-05';
-import { Lesson06 } from '../components/Lesson-06/Lesson-06';
-import { Lesson07 } from '../components/Lesson-07/Lesson-07';
-import { Lesson08 } from '../components/Lesson-08/Lesson-08';
 
-import styles from './MenuApp.module.css';
+import styles from './NavBar.module.css';
 import clsx from 'clsx';
 
-export const MenuApp = () => {
+export const NavBar = () => {
   const [activeLink, setActiveLink] = useState('/');
 
   const handleNavLinkClick = to => {
@@ -91,16 +83,6 @@ export const MenuApp = () => {
           Lesson08
         </NavLink>
       </nav>
-      <Routes>
-        <Route path="/" element={<Lesson01 />} />
-        <Route path="/lesson02" element={<Lesson02 />} />
-        <Route path="/lesson03" element={<Lesson03 />} />
-        <Route path="/lesson04" element={<Lesson04 />} />
-        <Route path="/lesson05" element={<Lesson05 />} />
-        <Route path="/lesson06" element={<Lesson06 />} />
-        <Route path="/lesson07" element={<Lesson07 />} />
-        <Route path="/lesson08" element={<Lesson08 />} />
-      </Routes>
     </div>
   );
 };
